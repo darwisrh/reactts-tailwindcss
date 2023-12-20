49dd8ff8c84d7d8bbc7ff45cc4c85fad799f15d1
Darwis <107021419+darwisrh@users.noreply.github.com>
2023-12-20 10:26:57 +0700

    reactts tailwind

---
+++ README.md
@@ -0,0 +1,12 @@
+## ReactTS and TailwindCSS starte
+First git clone this repository and then :
+```
+npm i
+```
+on your terminal, and
+```
+npm run dev
+```
+to start.
+
+For more information and documentation about tailwindcss visit => [tailwindcss.com](https://tailwindcss.com/docs)
--- src/pages/landing.tsx
+++ src/pages/landing.tsx
@@ -2,12 +2,18 @@
 
 const Landing: React.FC = () => {
    return (
-      <div>
-         <h1
-            className="text-blue-900 bg-yellow-700"
+      <div 
+         className="h-[100vh] bg-[#232946] flex justify-center items-center"
+      >
+         <div
+            className="bg-[#b8c1ec] w-[500px] h-[300px] rounded-lg flex justify-center items-center border-[5px] border-[#121629]"
          >
-            WASSUP BOII
-         </h1>
+            <h1
+               className="text-[30px] font-[700] text-center"
+            >
+               This page created using React Typescript and TailwindCSS
+            </h1>
+         </div>
       </div>
    )
 }
